b9ae6b003ed89421a37482b1646ab5bcdda2336d
Anton Korzunov <thekashey@gmail.com>
2022-04-18 11:48:52 +1000

    update infrastructure

---
+++ .size-limit.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    path: 'dist/es2015/index.js',
+    limit: '0.4 KB',
+  },
+  {
+    path: 'dist/es2015/useRef.js',
+    limit: '0.2 KB',
+  },
+  {
+    path: 'dist/es2015/useMergeRef.js',
+    limit: '0.2 KB',
+  },
+];
