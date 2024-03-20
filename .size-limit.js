a08e3804ce3ae19f8131693a0e3208b87d61c177
Anton Korzunov <thekashey@gmail.com>
2024-03-20 22:07:55 +1100

    commit size change

--- .size-limit.js
+++ .size-limit.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
     path: 'dist/es2015/index.js',
-    limit: '0.5 KB',
+    limit: '0.8 KB',
   },
   {
     path: 'dist/es2015/useRef.js',
-    limit: '0.2 KB',
+    limit: '0.25 KB',
   },
   {
     path: 'dist/es2015/useMergeRef.js',
-    limit: '0.3 KB',
+    limit: '0.65 KB',
   },
 ];
